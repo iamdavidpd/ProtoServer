ae4912d523b4332948f8278c6462b86cda8f37ea
Juan David Perez <jd.perezd1@uniandes.edu.co>
2025-04-22 18:57:35 -0500

    Implement Servicio class with properties and methods for id, nombre, ip, and puerto

--- src/Servidores/Servicio.java
+++ src/Servidores/Servicio.java
@@ -1,5 +1,48 @@
 package Servidores;
 
 public class Servicio {
+    private String id;
+    private String nombre;
+    private String ip;
+    private int puerto;
+
+    public Servicio(String id, String nombre, String ip, int puerto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ip = ip;
+        this.puerto = puerto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(int puerto) {
+        this.puerto = puerto;
+    }
 
 }
